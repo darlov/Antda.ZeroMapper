49ed9f1af3a2c767adcd5b9a81439a2f32887ba6
Dzianis Arlouski <tranquilfirst@gmail.com>
2023-01-11 13:10:21 +0300

    Initial commit

---
+++ src/ZeroMapper/ZeroMapper/ZeroMapperGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace ZeroMapper;
+
+[Generator]
+public class ZeroMapperGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
